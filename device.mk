afdc9504a0c22dadf48a5894cc2a87a05add151d
Verevka <verevka86@gmail.com>
2017-08-25 14:45:11 +0300

    add shim libimsmedia_jni.so

--- device.mk
+++ device.mk
@@ -200,6 +200,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sec_config:system/etc/sec_config
 
+# Libshims
+PRODUCT_PACKAGES += \
+    libshims_imsmedia
 
 # Lights
 PRODUCT_PACKAGES += \
