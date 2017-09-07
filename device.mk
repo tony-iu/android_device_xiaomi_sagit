eab49bcd38de507d57b87a63bbc582a5d872f202
Verevka <verevka86@gmail.com>
2017-09-07 09:09:18 +0300

    fix gapps

--- device.mk
+++ device.mk
@@ -242,7 +242,7 @@ PRODUCT_COPY_FILES += \
 
 # Open gapps
 GAPPS_VARIANT := pico
-GAPPS_FORCE_MATCHING_DPI := true
+#GAPPS_FORCE_MATCHING_DPI := true
 GAPPS_FORCE_BROWSER_OVERRIDES := true
 $(call inherit-product, vendor/opengapps/build/opengapps-packages.mk)
 
