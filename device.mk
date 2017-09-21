ae3f5e8c90016b053004e448e59cdcd8a18881c4
xiaobai <fengz181x@126.com>
2017-09-21 22:57:05 +0800

    sagit: build livedisplay jni

--- device.mk
+++ device.mk
@@ -219,6 +219,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     lights.msm8998
 
+# LiveDisplay jni
+PRODUCT_PACKAGES += \
+    libjni_livedisplay
+
 # Media
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media_codecs.xml:system/etc/media_codecs.xml \
